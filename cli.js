8ab238e797e6b52a294628b03074e0bdd94e659f
Yulu_Pan <panyulu2000@gmail.com>
2022-10-13 12:14:26 -0400

    fix

--- cli.js
+++ cli.js
@@ -26,12 +26,12 @@ var long = args.e ? args.e : args.w;
 var day = args.d ? args.d : 1;
 var start = moment().format("YYYY-MM-DD"); 
 var end = moment().add(day,'days').format("YYYY-MM-DD");
-var url = "https://api.open-meteo.com/v1/forecast?latitude="+lat+"&longitude="+long+"&daily=precipitation_hours&temperature_unit=fahrenheit&timezone="+timezone+"&start_date="+start+"&end_date="+end;
+var url = "https://api.open-meteo.com/v1/forecast?latitude="+lat+"&longitude="+long+"&daily=precipitation_hours&temperature_unit=fahrenheit&timezone="+timezone+"&start_date="+start+"&end_date="+end+",precipitation_hours";
 const response = await fetch(url);
 const data = await response.json();
 const days = args.d; 
 
-if (data["daily"]["precipitation"][days] != 0) {
+if (data["daily"]["precipitation_hours"][days] != 0) {
     console.log("You might need your galoshes");
   } else {
     console.log("You will not need your galoshes");
